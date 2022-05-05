699cdf02ea6519db138e9ad6183826e6eb0885fa
touhidurrr <46617994+touhidurrr@users.noreply.github.com>
2022-05-05 20:08:56 +0600

    fix `Discord.createMessage`

--- src/modules/Discord.js
+++ src/modules/Discord.js
@@ -1,10 +1,11 @@
 const apiEndpoint = 'https://discord.com/api/v10';
 
 exports.createMessage = async (channelId, message) => {
-  await fetch(`${apiEndpoint}/channels/${channelId}/messages`, {
+  return await fetch(`${apiEndpoint}/channels/${channelId}/messages`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
+      'Content-Type': 'application/json',
       Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
     },
     body: JSON.stringify(message),
