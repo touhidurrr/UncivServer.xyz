99f151269cf2c057036dcb9a6e7d39eeac21b21a
touhidurrr <46617994+touhidurrr@users.noreply.github.com>
2022-05-05 19:50:13 +0600

    fix malformed url at `Discord.createMessage`

--- src/modules/Discord.js
+++ src/modules/Discord.js
@@ -1,7 +1,7 @@
 const apiEndpoint = 'https://discord.com/api/v10';
 
 exports.createMessage = async (channelId, message) => {
-  await fetch(`apiEndpoint/channels/${channelId}/messages`, {
+  await fetch(`${apiEndpoint}/channels/${channelId}/messages`, {
     headers: {
       Accept: 'application/json',
       Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
