4e0c256f7ec44e2e990142529494359fd2c4a6bb
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-06-03 15:12:14 +0600

    Disable multiple server sync for now

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const { rm, stat, mkdir, readdir, rmSync, writeFileSync } = require('fs');
 
 // Battle Royale Games
 var BattleRoyaleGames = new Set();
-const ServerList = process.env.Servers.split(/[\n\s]+/);
+//const ServerList = process.env.Servers.split(/[\n\s]+/);
 
 // error logger
 const errorLogger = e => console.error(e.stack);
@@ -158,9 +158,9 @@ server.put('/files/:fileName', async (req, res) => {
   if (BattleRoyaleGames.has(req.params.fileName)) handleBRGame(req);
 
   writeFileSync(req.path.slice(1), req.body);
-  ServerList.forEach(endpoint => {
+  /*ServerList.forEach(endpoint => {
     fetch(`http://${endpoint}/files/${req.params.fileName}`, { method: 'PATCH', body: req.body });
-  });
+  });*/
   await server.locals.db.UncivServer.updateOne(
     { _id: req.params.fileName },
     { $set: { timestamp: Date.now(), text: req.body } },
