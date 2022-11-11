a933ae223f8c8e4949600535904861ea3fb458f7
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-11-11 06:55:20 +0600

    use `REDIS_URL` env for Redis

--- src/plugins/Redis.ts
+++ src/plugins/Redis.ts
@@ -1,7 +1,10 @@
 import fp from 'fastify-plugin';
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+
 client.on('error', err => console.log('Redis Client Error', err));
 
 // type definations
