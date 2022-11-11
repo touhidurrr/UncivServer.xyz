6653d83f902c8bd4de62b32676d055cdd3ef4a03
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-11-12 01:32:50 +0600

    better name collision prevention in FileServer

--- src/plugins/FileServer.ts
+++ src/plugins/FileServer.ts
@@ -12,51 +12,47 @@ declare module 'fastify' {
 
 export default fp(async function (server) {
   // populate cache from 'public' directory once
-  const { redis, filesDir, defaultType, expireAfter } = server;
+  (function populateFromDirectory() {
+    const sliceLength = server.filesDir.length;
 
-  function populateFromDirectory(dirName: string, cache: typeof redis) {
-    const sliceLength = dirName.length;
-
-    glob(dirName + '/**', { nodir: true }, async (err, fileList) => {
+    glob(server.filesDir + '/**', { nodir: true }, async (err, fileList) => {
       if (err) server.errorLogger(err);
 
       for (let fileName of fileList) {
         try {
           const fileBody = readFileSync(fileName);
           const uriPath = fileName.slice(sliceLength);
-          await cache.set(uriPath, fileBody, { EX: expireAfter });
+          await server.redis.set(uriPath, fileBody, { EX: server.expireAfter });
         } catch (err) {
           server.errorLogger(err);
         }
       }
     });
-  }
-
-  populateFromDirectory(filesDir, redis);
+  })();
 
   // make directory 'files'
-  mkdir(`${filesDir}/files`, err => err && server.errorLogger(err));
+  mkdir(`${server.filesDir}/files`, err => err && server.errorLogger(err));
 
   server.addHook('onRequest', async function (req, reply) {
     // set default contentType if none is available
     // or the app sends some silly header in PUT requests
     if (!req.raw.headers['content-type'] || req.method === 'PUT') {
-      req.raw.headers['content-type'] = defaultType;
+      req.raw.headers['content-type'] = this.defaultType;
     }
 
     // set fileName & type
     // append 'index.html' if url ends with '/'
-    req.fileName = filesDir + req.url;
+    req.fileName = this.filesDir + req.url;
     if (req.url.at(-1) === '/') {
       req.fileName += 'index.html';
     }
 
     // determine contentType from fileName
-    reply.type(mime.lookup(req.fileName) || defaultType);
+    reply.type(mime.lookup(req.fileName) || this.defaultType);
 
     if (req.method === 'GET') {
       // lookup in redis cache
-      const cachedBody = await redis.get(req.url).catch(console.error);
+      const cachedBody = await this.redis.get(req.url).catch(this.errorLogger);
 
       // if cache is found then return it
       if (cachedBody) {
@@ -68,7 +64,7 @@ export default fp(async function (server) {
       if (!req.url.startsWith('/files/') && existsSync(req.fileName)) {
         try {
           const fileBody = readFileSync(req.fileName);
-          await this.redis.set(req.url, fileBody, { EX: expireAfter });
+          await this.redis.set(req.url, fileBody, { EX: this.expireAfter });
           reply.send(fileBody);
           return reply;
         } catch (err) {
