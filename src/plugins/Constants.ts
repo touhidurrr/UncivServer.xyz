966132601d4677f8d76cbf94a49838801aec21ab
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2023-07-01 02:13:41 +0600

    reduce expireAfter to 10 minutes

--- src/plugins/Constants.ts
+++ src/plugins/Constants.ts
@@ -9,32 +9,20 @@ declare module 'fastify' {
     defaultType: string;
     gameFileRegex: RegExp;
     errorLogger: typeof errorLogger;
-    authVersion: typeof authVersion;
-    isAliveText: typeof isAliveText;
   }
 }
 
 // constants
-export const publicDir = 'public';
-export const expireAfter = 15 * 60; // expiration in seconds
-export const defaultType = mime.contentType('text/plain') as string;
-export const gameFileRegex = /^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}(_Preview)?$/;
-export const errorLogger = (e: any): void => console.error(e?.stack ?? e);
+const publicDir = 'public';
+const expireAfter = 10 * 60; // expiration in seconds
+const defaultType = mime.contentType('text/plain') as string;
+const gameFileRegex = /^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}(_Preview)?$/;
+const errorLogger = (e: any) => e && console.error(e?.stack);
 
-export const authVersion = 1 as const;
-export const isAliveText = JSON.stringify({
-  authVersion: 1,
-}) as `{"authVersion":${typeof authVersion}}`;
-
-export default fp(
-  async function (server) {
-    server.decorate('publicDir', publicDir);
-    server.decorate('expireAfter', expireAfter);
-    server.decorate('defaultType', defaultType);
-    server.decorate('errorLogger', errorLogger);
-    server.decorate('gameFileRegex', gameFileRegex);
-    server.decorate('authVersion', authVersion);
-    server.decorate('isAliveText', isAliveText);
-  },
-  { name: 'Constants' }
-);
+export default fp(async function (server) {
+  server.decorate('publicDir', publicDir);
+  server.decorate('expireAfter', expireAfter);
+  server.decorate('defaultType', defaultType);
+  server.decorate('errorLogger', errorLogger);
+  server.decorate('gameFileRegex', gameFileRegex);
+});
