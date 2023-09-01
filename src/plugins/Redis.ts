d03f37babb9ad3170c63a4e182b3234a2c47c86f
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2023-09-01 17:22:24 +0600

    Update Redis.ts

--- src/plugins/Redis.ts
+++ src/plugins/Redis.ts
@@ -2,10 +2,13 @@ import fp from 'fastify-plugin';
 import { createClient } from 'redis';
 
 const client = createClient({
-  url: process.env.REDIS_URL,
+  url: process.env.REDISCLOUD_URL || process.env.REDIS_URL,
 });
 
 client.on('error', err => console.log('Redis Client Error', err));
+client.on('ready', () => {
+  console.log('Redis Server started on:', client.options?.url)
+});
 
 // type definations
 declare module 'fastify' {
