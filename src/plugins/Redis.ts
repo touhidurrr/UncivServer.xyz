9616caeb66a694abe8885c1f9a0045c45d898fa8
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2023-09-01 17:22:24 +0600

    Update Redis.ts

--- src/plugins/Redis.ts
+++ src/plugins/Redis.ts
@@ -1,49 +1,24 @@
 import fp from 'fastify-plugin';
 import { createClient } from 'redis';
-import { expireAfter } from './Constants';
 
-const filesCacheClient = createClient({
-  url: process.env.REDIS_URL,
-  database: 0,
+const client = createClient({
+  url: process.env.REDISCLOUD_URL || process.env.REDIS_URL,
 });
 
-const authCacheClient = createClient({
-  url: process.env.REDIS_URL,
-  database: 1,
+client.on('error', err => console.log('Redis Client Error', err));
+client.on('ready', () => {
+  console.log('Redis Server started on:', client.options?.url)
 });
-
-filesCacheClient.on('error', err => console.log('Redis Client Error', err));
-authCacheClient.on('error', err => console.log('Redis Client Error', err));
 
 // type definations
 declare module 'fastify' {
   interface FastifyInstance {
-    cache: typeof cache;
+    redis: typeof client;
   }
 }
 
-const cache = {
-  files: {
-    get: (url: string) => filesCacheClient.get(url),
-    del: (url: string) => filesCacheClient.del(url),
-    set: (url: string, data: string | Buffer, { sec, ms }: { sec?: number; ms?: number } = {}) =>
-      filesCacheClient.set(url, data, ms ? { PX: ms } : { EX: sec ?? expireAfter }),
-    client: filesCacheClient,
-  },
-  auth: {
-    get: (userId: string) => authCacheClient.get(userId),
-    set: (userId: string, hash: string) => authCacheClient.set(userId, hash),
-    client: authCacheClient,
-  },
-};
-
-export default fp(
-  async function (server) {
-    // connect redis clients
-    await Promise.all([filesCacheClient.connect(), authCacheClient.connect()]);
-
-    server.decorate('cache', cache);
-    console.log('Loaded Redis Plugin!');
-  },
-  { name: 'Redis' }
-);
+export default fp(async function (server) {
+  await client.connect();
+  server.decorate('redis', client);
+  console.log('Loaded Redis Plugin!');
+});
