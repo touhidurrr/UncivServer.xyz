e48c0ac9d4342e0c950a9e0465cf4c70dffa82fe
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-10-11 00:23:33 +0600

    fix `parseUncivGameData`

--- src/lib/parseUncivGameData.ts
+++ src/lib/parseUncivGameData.ts
@@ -3,6 +3,7 @@ import type { UncivJSON } from '@localTypes/unciv';
 const dec = new TextDecoder();
 
 export function parseUncivGameData(data: string): UncivJSON {
-  const jsonArray = Bun.gunzipSync(Buffer.from(data, 'base64').toString('utf8'));
+  //@ts-ignore
+  const jsonArray = Bun.gunzipSync(Buffer.from(data, 'base64'));
   return JSON.parse(dec.decode(jsonArray));
 }
