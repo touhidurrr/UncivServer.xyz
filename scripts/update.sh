e886c5cc827c968b3a7d24db9a2af5046992369a
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-12-18 18:30:10 +0600

    update scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-PROJECT_DIR=`dirname $(dirname $0)`
+# Setup
+PROJECT_DIR="/path/to/project/dir"
+BUN_DIR="/path/to/bun"
+
+# Update
+alias bun="$BUN_DIR/.bin/bun"
 cd "$PROJECT_DIR"
 
 # Check for updates
@@ -10,6 +15,7 @@ git fetch
 # If diff avaiable
 if ! git diff --quiet origin/main; then
     echo "Updates found! Restarting..."
+
     git pull
     bun upgrade && bun install --frozen-lockfile
     systemctl restart uncivserver
