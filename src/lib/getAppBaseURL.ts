6477bb6660eb26a39f1f435662b5f2ec10519495
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-12-24 02:30:49 +0600

    sync test patch 2

--- src/lib/getAppBaseURL.ts
+++ src/lib/getAppBaseURL.ts
@@ -1,4 +1,6 @@
 import { DEFAULT_PORT } from '@constants';
 import { hostname } from 'node:os';
 
-export const getAppBaseURL = () => `http://${hostname()}:${DEFAULT_PORT}` as const;
+const host = process.env.HOST ?? hostname();
+const port = process.env.PORT ?? DEFAULT_PORT;
+export const getAppBaseURL = () => `http://${host}:${port}` as const;
