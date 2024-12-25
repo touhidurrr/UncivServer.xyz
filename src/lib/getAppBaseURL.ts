5f876b1187fb494c855e72f6ad172e657923afee
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-12-25 20:38:27 +0600

    revert `getAppBaseURL()` to its previous behavior

--- src/lib/getAppBaseURL.ts
+++ src/lib/getAppBaseURL.ts
@@ -1,6 +1,4 @@
 import { DEFAULT_PORT } from '@constants';
 import { hostname } from 'node:os';
 
-const host = process.env.HOST ?? hostname();
-const port = process.env.PORT ?? DEFAULT_PORT;
-export const getAppBaseURL = () => `http://${host}:${port}` as const;
+export const getAppBaseURL = () => `http://${hostname()}:${DEFAULT_PORT}` as const;
