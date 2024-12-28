20a6d62d41cb697f811510d9166166520af1f86a
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-12-28 20:19:21 +0600

    some updates

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -2,7 +2,7 @@ import { createClient } from '@libsql/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql';
 import { PrismaClient } from '@prisma/client';
 
-const libsql = createClient({
+export const libsql = createClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
