8e376f92a2d0436fdcd6d633f6878373051109ee
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-12-29 22:57:37 +0600

    use `BigInt` and update `sqlgen` script

--- scripts/sqlgen.ts
+++ scripts/sqlgen.ts
@@ -1,17 +1,21 @@
-import { rm, cp } from 'fs/promises';
+import { cp, rm } from 'fs/promises';
+
+const PRISMA_FOLDER = 'prisma';
+const OUT_FILE = 'prisma/generated.sql';
 
 const cleanup = async () => {
-  const glob = new Bun.Glob('prisma/*.db**').scan({ onlyFiles: true });
+  const glob = new Bun.Glob(`${PRISMA_FOLDER}/**.db*`).scan({ onlyFiles: true });
 
   await Promise.all([
-    rm('prisma/migrations', { force: true, recursive: true }),
+    rm(`${PRISMA_FOLDER}/migrations`, { force: true, recursive: true }),
     Array.fromAsync(glob).then(files => Promise.all(files.map(file => rm(file)))),
   ]);
 };
 
 await cleanup();
 await Bun.$`prisma migrate dev --name temp`;
-await Array.fromAsync(new Bun.Glob('prisma/**/migration.sql').scan()).then(files =>
-  Promise.all(files.map(file => cp(file, 'prisma/generated.sql')))
+await Array.fromAsync(new Bun.Glob(`${PRISMA_FOLDER}/**/migration.sql`).scan()).then(files =>
+  Promise.all(files.map(file => cp(file, OUT_FILE)))
 );
+await Bun.$`prettier --write ${OUT_FILE}`;
 await cleanup();
