d8e8b706ea5e06895748eeb1835557923666cb5d
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-02-19 23:47:18 +0600

    fix `timestamp` not getting updated on `PUT /files/[id]`

--- src/routes/files/put.ts
+++ src/routes/files/put.ts
@@ -33,7 +33,7 @@ export const putFile = (app: Elysia) =>
         // save on mongodb
         await db.UncivGame.updateOne(
           { _id: gameId },
-          { $set: { text: body as string } },
+          { $set: { text: body as string, timestamp: Date.now() } },
           { upsert: true }
         ).catch(err => console.error(`[MongoDB] Error saving game ${gameId}:`, err));
 
