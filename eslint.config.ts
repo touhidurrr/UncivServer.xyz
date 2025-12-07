3d882225f404d91707777d2eb72a7f7eab953236
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-12-07 21:19:35 +0600

    cleanup `eslint.config.ts`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,9 @@
 import eslint from '@eslint/js';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   eslint.configs.recommended,
   tseslint.configs.strict,
-  globalIgnores(['test.ts', 'src/types/unciv.ts']),
-  { rules: { '@typescript-eslint/unified-signatures': 'off' } },
+  { ignores: ['test.ts', 'src/types/unciv.ts'] },
 ]);
