490d9f5ecce42366bd59a69cc0c2cf1fdd807e0e
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2026-02-08 07:08:42 +0600

    reduce default cache limits (#83)
    * red
    
    * Bump version from 4.51.1 to 4.52.0

--- src/constants.ts
+++ src/constants.ts
@@ -20,8 +20,8 @@ export const MAX_CONTENT_LENGTH = bytes.parse('2mb')!;
 export const REDIS_DEFAULT_URL = '0.0.0.0:6379';
 
 // Cache
-export const CACHE_MAX_ITEMS = 10_000;
-export const CACHE_MAX_SIZE = bytes.parse('150mb');
+export const CACHE_MAX_ITEMS = 5_000;
+export const CACHE_MAX_SIZE = bytes.parse('100mb');
 export const CACHE_TTL_SECONDS = 30 * 60;
 export const CACHE_TTL_MILLISECONDS = CACHE_TTL_SECONDS * 1000;
 export const CACHE_TTL_CHECK_INTERVAL_SECONDS = 60;
