5d7e1a0a3042f06515ff7460a360ea7a31f7bdea
Md. Touhidur Rahman <touhidurrr@pm.me>
2026-03-21 06:31:36 +0600

    fix broken site

--- src/index.ts
+++ src/index.ts
@@ -62,6 +62,7 @@ export const app = new Elysia({
       if (+contentLen > MAX_CONTENT_LENGTH) return status(413);
     }
   })
+  .use(staticPlugin({ prefix: '/', alwaysStatic: true }))
   .use(devPlugin)
   .use(statsRoute)
   .use(jwtPlugin)
@@ -79,7 +80,6 @@ export const app = new Elysia({
   })
   .all('/support', ctx => ctx.redirect(SUPPORT_URL, 303))
   .all('/discord', ctx => ctx.redirect(DISCORD_INVITE, 303))
-  .use(staticPlugin({ prefix: '/', alwaysStatic: true }))
   .listen(unix ? { unix } : { port, hostname }, server => {
     if (unix) chmod(unix, 0o666);
     console.log(`Server started at ${server.url}`);
