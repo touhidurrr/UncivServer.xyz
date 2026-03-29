2fadf562985e597bcc169507c53c443567ba5d87
Md. Touhidur Rahman <touhidurrr@pm.me>
2026-03-30 00:24:22 +0600

    fix bad markdown in support embed title

--- src/constants.ts
+++ src/constants.ts
@@ -93,7 +93,7 @@ export const SUPPORT_EMBED_MESSAGE = `Enjoying **UncivServer.xyz**? **[${SUPPORT
 
 export const SUPPORT_EMBED: Readonly<APIEmbed> = {
   author: {
-    name: 'Buy **touhidurrr** a Coffee!',
+    name: 'Buy touhidurrr a Coffee!',
     icon_url: 'https://media.giphy.com/media/TDQOtnWgsBx99cNoyH/giphy.gif',
   },
   description: SUPPORT_EMBED_MESSAGE,
